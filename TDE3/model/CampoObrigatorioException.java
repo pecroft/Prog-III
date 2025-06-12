770fa6ce01a507d194e7ca47334a2eb9cc7f6874
pecroft <pedro.rolim@soulasalle.com.br>
2025-06-12 19:58:56 -0300

    .

--- TDE3/model/CampoObrigatorioException.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class CampoObrigatorioException extends Exception {
-    public CampoObrigatorioException(String mensagem) {
-        super(mensagem);
-    }
-}
--- TDE3/model/CursoNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class CursoNaoEncontradoException extends Exception {
-    public CursoNaoEncontradoException(String mensagem) {
-        super(mensagem);
-    }
-}
